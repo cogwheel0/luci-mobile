c857176dcc1ac215784c770f0d0bde0d1d025e96
cogwheel <cogwheel@cogwheel.app>
2026-08-21 13:41:53 +0530

    chore(deps): upgrade to riverpod 3, secure_storage 11, package_info_plus 10
    - flutter_riverpod ^2.6.1 -> ^3.4.2 (ChangeNotifierProvider now via legacy import)
    - flutter_secure_storage ^9.2.4 -> ^11.0.0
    - package_info_plus ^8.3.0 -> ^10.2.1
    - add crypto ^3.0.7 (certificate fingerprint pinning)
    - minor bumps: dio, fl_chart, url_launcher, cupertino_icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 import 'package:luci_mobile/state/app_state.dart';
 import 'package:luci_mobile/screens/login_screen.dart';
